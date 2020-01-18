b0391771bb2bc6886a94dfa06f8ba795fed7bdd5
Jean Maillard <jean@up-ri.se>
2020-01-18 22:17:57 +0100

    add pwa config to suit icons and title of app

--- vue.config.js
+++ vue.config.js
@@ -8,4 +8,11 @@ module.exports = {
     //public: 'sachat.seed.fr.nf',
     proxy: 'https://localhost:8080',
   },
+  pwa: {
+    name: 'Sachat',
+    themeColor: '#4DBA87',
+    msTileColor: '#4DBA87',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#4DBA87',
+  }
 }
